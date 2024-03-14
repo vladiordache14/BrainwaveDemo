50e249ba1299be0f74234a65a595d87b15bd03e1
VladIordache <vlad.iordache@nexttech.ro>
2024-03-14 22:45:11 +0200

    ss

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -20,6 +20,7 @@ const Section =  ({
          <div className="hidden absolute top-0 left-5 w-0.25 h-full
          bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10"/>
 
+
          {crosses && (
             <>
                 <div className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 
